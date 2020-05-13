fd0d83efbef32fa3f89ab2ab59d0b3a6915324d3
vaddipar <vaddipar@adobe.com>
2020-05-13 19:03:40 +0530

    added unit test

---
+++ groclistcommons/src/test/java/com/groclist/groclistcommons/GrocListCommonsTest.java
@@ -0,0 +1,13 @@
+package com.groclist.groclistcommons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GrocListCommonsTest {
+
+    @Test
+    public void mergeIngredient() {
+        System.out.println("Random test");
+    }
+}
